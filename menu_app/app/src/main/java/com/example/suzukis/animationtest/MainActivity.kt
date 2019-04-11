928839cdd8b43c3d1d6b74167991954d665cd762
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-04-11 22:58:54 +0900

    add_close_animation

--- menu_app/app/src/main/java/com/example/suzukis/animationtest/MainActivity.kt
+++ menu_app/app/src/main/java/com/example/suzukis/animationtest/MainActivity.kt
@@ -8,12 +8,14 @@ import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.TextView
+import java.lang.Thread.sleep
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
 class MainActivity : AppCompatActivity() {
 
+    var menuFlg: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,60 +26,82 @@ class MainActivity : AppCompatActivity() {
         buttons.set(2, findViewById(R.id.button3))
         buttons.set(3, findViewById(R.id.button4))
         buttons.set(4, findViewById(R.id.button5))
-        val menuLayout :FrameLayout = findViewById(R.id.menuLayout)
+        val menuLayout: FrameLayout = findViewById(R.id.menuLayout)
         val textView: TextView = findViewById(R.id.test_textView)
         val testButton: Button = findViewById(R.id.test_Button)
 
         testButton.setOnClickListener(View.OnClickListener {
-            animateOpenMenu(buttons,menuLayout)
+            if (!menuFlg)
+                animateOpenMenu(buttons, menuLayout)
+            else
+                animateTranslateCloneMenu(buttons, menuLayout)
+
         })
 
         menuLayout.setOnClickListener(View.OnClickListener {
-            animateCloneMenu(buttons,menuLayout)
+            animateAlphaCloseMenu(menuLayout)
         })
     }
 
-    fun animateOpenMenu(buttons: Array<Button?>, menuLayout:FrameLayout) {
+    fun animateOpenMenu(buttons: Array<Button?>, menuLayout: FrameLayout) {
         val radius = 400f
-        val time :Long=300
+        val time: Long = 300
         val array = Array(5, { arrayOfNulls<PropertyValuesHolder>(2) })
         array[0][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(PI / 2).toFloat())
         array[0][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI / 2).toFloat())
         array[1][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(PI * 112.5 / 180).toFloat())
         array[1][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI * 112.5 / 180).toFloat())
-        array[2][0] = PropertyValuesHolder.ofFloat("translationX", 0f,radius * cos(PI * 135 / 180).toFloat())
+        array[2][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(PI * 135 / 180).toFloat())
         array[2][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI * 135 / 180).toFloat())
         array[3][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(PI * 157.5 / 180).toFloat())
         array[3][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI * 157.5 / 180).toFloat())
         array[4][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(1 * PI).toFloat())
         array[4][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(1 * PI).toFloat())
 
-        val menuLayoutAnimation = ObjectAnimator.ofFloat(menuLayout,"alpha",0f,1f)
+        val menuLayoutAnimation = ObjectAnimator.ofFloat(menuLayout, "alpha", 0f, 0.5f)
         menuLayoutAnimation.setDuration(time)
         menuLayoutAnimation.start()
         visiable(menuLayout)
-         for (i in array.indices) {
+        for (i in array.indices) {
             visiable(buttons[i])
             val objectAnimator: ObjectAnimator = ObjectAnimator.ofPropertyValuesHolder(buttons[i], array[i][0], array[i][1])
             objectAnimator.setDuration(time)
             objectAnimator.start()
         }
-
+        menuFlg = true
     }
 
-    fun animateCloneMenu(buttons:Array<Button?>,menuLayout: FrameLayout){
+    fun animateTranslateCloneMenu(buttons: Array<Button?>, menuLayout: FrameLayout) {
+        val radius = 400f
         val time: Long = 200
-//        val array = Array(5, { arrayOfNulls<PropertyValuesHolder>(2) })
-//        array[0][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(PI / 2).toFloat())
-//        array[0][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI / 2).toFloat())
-//        array[1][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(PI * 112.5 / 180).toFloat())
-//        array[1][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI * 112.5 / 180).toFloat())
-//        array[2][0] = PropertyValuesHolder.ofFloat("translationX", 0f,radius * cos(PI * 135 / 180).toFloat())
-//        array[2][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI * 135 / 180).toFloat())
-//        array[3][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(PI * 157.5 / 180).toFloat())
-//        array[3][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(PI * 157.5 / 180).toFloat())
-//        array[4][0] = PropertyValuesHolder.ofFloat("translationX", 0f, radius * cos(1 * PI).toFloat())
-//        array[4][1] = PropertyValuesHolder.ofFloat("translationY", 0f, -radius * sin(1 * PI).toFloat())
+        val array = Array(5, { arrayOfNulls<PropertyValuesHolder>(2) })
+        array[0][0] = PropertyValuesHolder.ofFloat("translationX", radius * cos(PI / 2).toFloat(), 0f)
+        array[0][1] = PropertyValuesHolder.ofFloat("translationY", -radius * sin(PI / 2).toFloat(), 0f)
+        array[1][0] = PropertyValuesHolder.ofFloat("translationX", radius * cos(PI * 112.5 / 180).toFloat(), 0f)
+        array[1][1] = PropertyValuesHolder.ofFloat("translationY", -radius * sin(PI * 112.5 / 180).toFloat(), 0f)
+        array[2][0] = PropertyValuesHolder.ofFloat("translationX", radius * cos(PI * 135 / 180).toFloat(), 0f)
+        array[2][1] = PropertyValuesHolder.ofFloat("translationY", -radius * sin(PI * 135 / 180).toFloat(), 0f)
+        array[3][0] = PropertyValuesHolder.ofFloat("translationX", radius * cos(PI * 157.5 / 180).toFloat(), 0f)
+        array[3][1] = PropertyValuesHolder.ofFloat("translationY", -radius * sin(PI * 157.5 / 180).toFloat(), 0f)
+        array[4][0] = PropertyValuesHolder.ofFloat("translationX", radius * cos(1 * PI).toFloat(), 0f)
+        array[4][1] = PropertyValuesHolder.ofFloat("translationY", -radius * sin(1 * PI).toFloat(), 0f)
+        for (i in array.indices) {
+            visiable(buttons[i])
+            val objectAnimator: ObjectAnimator = ObjectAnimator.ofPropertyValuesHolder(buttons[i], array[i][0], array[i][1])
+            objectAnimator.setDuration(time)
+            objectAnimator.start()
+        }
+        animateAlphaCloseMenu(menuLayout)
+    }
+
+    fun animateAlphaCloseMenu(menuLayout: FrameLayout) {
+        val time: Long = 200
+
+        val objectAnimator = ObjectAnimator.ofFloat(menuLayout, "alpha", 0.5f, 0f)
+        objectAnimator.setDuration(time)
+        objectAnimator.start()
+//        gone(menuLayout)
+        menuFlg = false
     }
 
     fun visiable(view: View?) {
@@ -86,7 +110,8 @@ class MainActivity : AppCompatActivity() {
                 view.visibility = View.VISIBLE
         }
     }
-    fun gone(view: View?){
+
+    fun gone(view: View?) {
         if (view != null) {
             if (view.visibility == View.VISIBLE)
                 view.visibility = View.GONE
